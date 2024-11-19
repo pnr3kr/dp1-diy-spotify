0ad0e38312d010e0ea483f0a0774c519996ff107
Tristen Davin <pnr3kr@virginia.edu>
2024-11-18 19:52:19 -0500

    final changes for the day

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ import mysql.connector
 from mysql.connector import Error
 
 # Database connection details
-DBHOST = "ds2022.cqee4iwdcaph.us-east-1.rds.amazonaws.com"
-DBUSER = "admin"
+DBHOST = os.getenv('DBHOST')
+DBUSER = os.getenv('DBUSER')
 DBPASS = os.getenv('DBPASS')
-DB = "pnr3kr"
+DB = os.getenv('DB')
 
 try:
     db = mysql.connector.connect(
@@ -37,20 +37,20 @@ def get_genres():
     query = "SELECT * FROM genres ORDER BY genreid;"
     try:
         cur.execute(query)
-        headers=[x[0] for x in cur.description]
+        headers = [x[0] for x in cur.description]
         results = cur.fetchall()
-        json_data=[]
+        json_data = []
         for result in results:
-            json_data.append(dict(zip(headers,result)))
-        return(json_data)
+            json_data.append(dict(zip(headers, result)))
+        return json_data
     except Error as e:
         return {"Error": "MySQL Error: " + str(e)}
 
 @app.get('/songs')
 def get_songs():
     query = """
-        SELECT songs.title, songs.album, songs.artist, songs.year, 
-               songs.file, songs.image, genres.genre
+        SELECT songs.id, songs.title, songs.album, songs.artist,
+               genres.genre AS genre, songs.year, songs.file, songs.image
         FROM songs
         JOIN genres ON songs.genre = genres.genreid
         ORDER BY songs.title;
@@ -63,6 +63,4 @@ def get_songs():
         return json_data
     except Error as e:
         return {"Error": f"MySQL Error: {e}"}
-
-
 
